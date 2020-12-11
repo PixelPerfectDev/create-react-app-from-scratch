8169df781c7a960a562228c405e594ecadd55c42
PixelPerfectDev <joshbowdenconcepts@gmail.com>
2020-12-10 23:14:58 -0500

    updated to use functional components

--- src/index.js
+++ src/index.js
@@ -1,24 +1,15 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import './styles/css/index.css';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import "./styles/css/index.css";
 
 // Registering the service worker
-import runtime from 'serviceworker-webpack-plugin/lib/runtime';
-if ('serviceWorker' in navigator) {
-    const registration = runtime.register();
+import runtime from "serviceworker-webpack-plugin/lib/runtime";
+if ("serviceWorker" in navigator) {
+  const registration = runtime.register();
 }
 
-class App extends Component {
-    render() {
-        return (
-            <div>
-                Hello World!
-            </div>
-        );
-    };
-}
+const App = () => {
+  return <div>Hello World!</div>;
+};
 
-ReactDOM.render(
-    <App />,
-    document.getElementById('app')
-)
+ReactDOM.render(<App />, document.getElementById("app"));

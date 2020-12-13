29b54345905da7aa2b025274f5a47dbb2f9e7d01
PixelPerfectDev <joshbowdenconcepts@gmail.com>
2020-12-12 21:58:33 -0500

    Updated to brents liking

---
+++ src/Nav.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Nav = () => {
+  return (
+    <nav>
+      <p>Brent G</p>
+      <div>
+        <p>Plex</p>
+      </div>
+    </nav>
+  );
+};
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-import React, { Component } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
+
+import { Nav } from "./Nav";
 import "./styles/css/index.css";
 
 // Registering the service worker
@@ -8,8 +10,39 @@ if ("serviceWorker" in navigator) {
   const registration = runtime.register();
 }
 
-const App = () => {
-  return <div>Hello World!</div>;
+const App = ({ name }) => {
+  const buttons = [
+    {
+      name: "FoodSpinner",
+      link: "/FoodSpinner",
+    },
+    {
+      name: "Is it Wood?",
+      link: "/FoodSpinner",
+    },
+    {
+      name: "TD PUBG",
+      link: "/TDPUBG",
+    },
+  ];
+
+  return (
+    <div>
+      <Nav />
+      <p className={"test"}>Hi {name}!</p>
+      {buttons.map((item) => (
+        <InnerDiv key={item.name} name={item.name} link={item.link}></InnerDiv>
+      ))}
+    </div>
+  );
 };
 
-ReactDOM.render(<App />, document.getElementById("app"));
+const InnerDiv = ({ name, link }) => {
+  return (
+    <a href={link}>
+      <div className="inner">{name}</div>
+    </a>
+  );
+};
+
+ReactDOM.render(<App name={"Brent"} />, document.getElementById("app"));
